f31553a59df4c56c0e94efff4842b12a60220f07
Rfiftr <107876669+Rfiftr@users.noreply.github.com>
2024-01-25 12:19:51 +0200

    Update script.js
    add option download images

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ const imagesContainer = document.querySelector('.images')
 const folderPath = './images';
 
 function createImages(index) {
-    imagesContainer.innerHTML += `
-        <div class="image-container">
-            <img src="images/ai-image-${index}.jpg" alt="Image ${index}"/>
-            <div class="overlay">
-                <button class="download-btn">Download</button>
-            </div>
-        </div>
-    `
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+
+    const image = document.createElement('img');
+    image.src = `images/ai-image-${index}.jpg`;
+    image.alt = `Image ${index}`;
+
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+    const downloadButton = document.createElement('button');
+    downloadButton.classList.add('download-btn');
+    downloadButton.innerText = 'Download';
+
+    downloadButton.addEventListener('click', () => {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = `images/ai-image-${index}.jpg`;
+        downloadLink.download = `Image${index}.jpg`;
+        downloadLink.click();
+    });
+
+    overlay.appendChild(downloadButton);
+    imageContainer.appendChild(image);
+    imageContainer.appendChild(overlay);
+    imagesContainer.appendChild(imageContainer)
 }
 
-for(let i = 1; i <= 46; i++) {
+for(let i = 1; i <= 65; i++) {
     createImages(i);
 }
-
-
-//const imagesSrc = getImageNamesAsync(folderPath)
-
